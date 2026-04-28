47959d29e8c0092ac2842961a973653da103593c
e10withadot <e10withadot@gmail.com>
2026-04-28 11:48:11 +0300

    parametized http requests to be more generic

--- create.go
+++ create.go
@@ -16,42 +16,8 @@ func create(args []string) {
     }
     pwd := args[0] + "/.devcontainer"
 
-    // curl -s -H "Accept: application/vnd.github+json" "https://api.github.com/repos/devcontainers/images/git/trees/HEAD?recursive=1"
-    var err error
-    var request *http.Request
-    client := &http.Client{}
-    if request, err = http.NewRequest("GET", "https://api.github.com/repos/devcontainers/images/git/trees/HEAD?recursive=1", nil); err != nil {
-        log.Fatal(err)
-    }
-    request.Header.Add("Accept", "application/vnd.github+json")
-    var response *http.Response
-    if response, err = client.Do(request); err != nil {
-        log.Fatal(err)
-    }
-    defer response.Body.Close()
-
-    // jq -r '.tree[].path'
-    var dc_dict struct {
-        Tree []struct {
-            Path string `json:"path"`
-            Mode string `json:"mode"`
-        } `json:"tree"`
-    }
-    if err := json.NewDecoder(response.Body).Decode(&dc_dict); err != nil {
-        log.Fatal(err)
-    }
-    urls := make([]huh.Option[string], 0)
-    for _, item := range dc_dict.Tree {
-        if strings.HasPrefix(item.Path, "src/") && item.Mode == "040000" {
-            var name string
-            if name = strings.TrimPrefix(item.Path, "src/"); !strings.Contains(name, "/") {
-                urls = append(urls, huh.NewOption(
-                    name,
-                    "mcr.microsoft.com/devcontainers/" + name,
-                ))
-            }
-        }
-    }
+    def_repo := "mcr.microsoft.com/devcontainers/"
+    urls := get_container_urls("https://api.github.com/repos/devcontainers/images/git/trees/HEAD?recursive=1", def_repo)
 
     var compose bool; var repo string
     form := huh.NewForm(
@@ -71,33 +37,12 @@ func create(args []string) {
             Value(&repo),
         ),
     )
-    if err = form.Run(); err != nil {
+    if err := form.Run(); err != nil {
         log.Fatal(err)
     }
 
-    // curl -s "https://mcr.microsoft.com/v2/devcontainers/${repo}/tags/list"
-    repo_name := strings.TrimPrefix(repo, "mcr.microsoft.com/devcontainers/")
-    if request, err = http.NewRequest("GET", "https://mcr.microsoft.com/v2/devcontainers/" + repo_name + "/tags/list", nil); err != nil {
-        log.Fatal(err)
-    }
-    request.Header.Add("Accept", "application/vnd.github+json")
-    if response, err = client.Do(request); err != nil {
-        log.Fatal(err)
-    }
-    defer response.Body.Close()
-
-    // jq -r '.tags[]'
-    var tag_dict struct {
-        Tags []string `json:"tags"`
-    }
-    if err := json.NewDecoder(response.Body).Decode(&tag_dict); err != nil {
-        log.Fatal(err)
-    }
-    def_tag := tag_dict.Tags[0]
-    tag_opts := make([]huh.Option[string], 0)
-    for _, tag := range tag_dict.Tags {
-        tag_opts = append(tag_opts, huh.NewOption(tag, tag))
-    }
+    repo_name := strings.TrimPrefix(repo, def_repo)
+    def_tag, tag_opts := get_container_tags(repo_name, "https://mcr.microsoft.com/v2/devcontainers/")
  
     var use_def bool
     form = huh.NewForm(
@@ -109,7 +54,7 @@ func create(args []string) {
             Value(&use_def),
         ),
     )
-    if err = form.Run(); err != nil {
+    if err := form.Run(); err != nil {
         log.Fatal(err)
     }
 
@@ -123,17 +68,15 @@ func create(args []string) {
                 Value(&version),
             ),
         )
-        if err = form.Run(); err != nil {
+        if err := form.Run(); err != nil {
             log.Fatal(err)
         }
     }
 
-    // mkdir -p .devcontainer
-    if err = os.Mkdir(pwd, 0750); err != nil && !os.IsExist(err) {
+    if err := os.Mkdir(pwd, 0750); err != nil && !os.IsExist(err) {
         log.Fatal(err)
     }
 
-    // printf "FROM %s\nRUN %s\n" "mcr.micrsoft.com/devcontainers/$repo:$version" "$DC_RUN" > .devcontainer/Dockerfile
     var dc_link string
     if compose {
         os.WriteFile(pwd + "/compose.yml", []byte("services:\n    main:\n        image: " + repo + ":" + version + "\n        command: sleep infinity"), 0666)
@@ -144,4 +87,65 @@ func create(args []string) {
     }
     os.WriteFile(pwd + "/devcontainer.json", []byte(dc_link), 0666)
     // DC_RUN="$(gum input --placeholder="Enter initial command...")"
+}
+
+func get_container_urls(repo string, image_repo string) []huh.Option[string] {
+    var err error
+    var request *http.Request; var response *http.Response
+    if request, err = http.NewRequest("GET", repo, nil); err != nil {
+        log.Fatal(err)
+    }
+    client := &http.Client{}
+    if response, err = client.Do(request); err != nil {
+        log.Fatal(err)
+    }
+    defer response.Body.Close()
+
+    var dc_dict struct {
+        Tree []struct {
+            Path string `json:"path"`
+            Mode string `json:"mode"`
+        } `json:"tree"`
+    }
+    if err = json.NewDecoder(response.Body).Decode(&dc_dict); err != nil {
+        log.Fatal(err)
+    }
+    urls := make([]huh.Option[string], 0)
+    for _, item := range dc_dict.Tree {
+        if strings.HasPrefix(item.Path, "src/") && item.Mode == "040000" {
+            var name string
+            if name = strings.TrimPrefix(item.Path, "src/"); !strings.Contains(name, "/") {
+                urls = append(urls, huh.NewOption(
+                    name,
+                    image_repo + name,
+                ))
+            }
+        }
+    }
+    return urls
+}
+
+func get_container_tags(image string, repo string) (string, []huh.Option[string]) {
+    var request *http.Request; var response *http.Response; var err error
+    if request, err = http.NewRequest("GET", repo + image + "/tags/list", nil); err != nil {
+        log.Fatal(err)
+    }
+    client := &http.Client{}
+    if response, err = client.Do(request); err != nil {
+        log.Fatal(err)
+    }
+    defer response.Body.Close()
+
+    var tag_dict struct {
+        Tags []string `json:"tags"`
+    }
+    if err = json.NewDecoder(response.Body).Decode(&tag_dict); err != nil {
+        log.Fatal(err)
+    }
+    def_tag := tag_dict.Tags[0]
+    tag_opts := make([]huh.Option[string], 0)
+    for _, tag := range tag_dict.Tags {
+        tag_opts = append(tag_opts, huh.NewOption(tag, tag))
+    }
+    return def_tag, tag_opts
 }
