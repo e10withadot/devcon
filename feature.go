cfbcdeef1c2cc82227996eac55e252594db815c2
e10withadot <e10withadot@gmail.com>
2026-08-18 13:02:27 +0300

    added feature cmd shell

--- feature.go
+++ feature.go
@@ -6,19 +6,23 @@ import (
     "os"
 )
 
-func main() {
-    if len(os.Args) < 2 {
+func feature(args []string) {
+    if len(args) < 1 {
         log.Fatal("Subcommand required.")
     }
     cargs := os.Args[2:]
     switch cmd := os.Args[1]; cmd {
-    case "create":
-        create(cargs)
-    case "up":
-        up(cargs)
-    case "feature":
-        feature(cargs)
+    case "add":
+        add(cargs)
+    case "rm":
+        rm(cargs)
     default:
         fmt.Println("Invalid command:", cmd)
     }
+}
+
+func add(args []string) {
+}
+
+func rm(args []string) {
 }
