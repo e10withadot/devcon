5886eaad365ec2778f37cd9d5928d9437bb0aafb
e10withadot <e10withadot@gmail.com>
2026-08-18 21:32:21 +0300

    devcon feature add has been roughly implemented

--- feature.go
+++ feature.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "os"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
 )
+
 
 func feature(args []string) {
     if len(args) < 1 {
@@ -23,12 +25,32 @@ func feature(args []string) {
 
 func add(args []string) {
     if len(args) < 2 {
-        log.Fatal("Requires 2 arguments: [feature] [path]")
+        log.Fatal("Correct syntax: devcon feature add [feature] [path]")
     }
+    var feature string = args[0]
+    var path string = args[1]
+    data, err := os.ReadFile(path + "/.devcontainer/devcontainer.json")
+    if err != nil {
+        log.Fatal("Could not read devcontainer.json.")
+    }
+    var dcj map[string]any
+    if err = json.Unmarshal(data, &dcj); err != nil {
+        log.Fatal("The devcontainer.json is invalid.")
+    }
+    features, ok := dcj["features"].([]any)
+    if !ok {
+        features = []any{}
+    }
+    features = append(features, map[string]any{ feature: map[string]any{}, })
+    dcj["features"] = features
+    if data, err = json.Marshal(&dcj); err != nil {
+        log.Fatal("Devcon could not reformat into json. Please report this error.")
+    }
+    os.WriteFile(path + "/.devcontainer/devcontainer.json", data, 0666)
 }
 
 func rm(args []string) {
     if len(args) < 2 {
-        log.Fatal("Requires 2 arguments: [feature] [path]")
+        log.Fatal("Correct syntax: devcon feature rm [feature] [path]")
     }
 }
